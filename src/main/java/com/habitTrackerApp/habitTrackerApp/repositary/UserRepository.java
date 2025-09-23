4101141b4e4e12ed9c6152b7839809f86d58d4b7
mr-arzh <asiris.joy@icloud.com>
2025-09-23 23:36:57 +0300

    feat: implemented loadUserByUsername() in UserSevice class

--- src/main/java/com/habitTrackerApp/habitTrackerApp/repositary/UserRepository.java
+++ src/main/java/com/habitTrackerApp/habitTrackerApp/repositary/UserRepository.java
@@ -5,10 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.habitTrackerApp.habitTrackerApp.*;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
-    UserEntity findUserByUsername(String username);
+    Optional<UserEntity> findUserByUsername(String username);
     UserEntity findUserByEmail(String email);
 
 
