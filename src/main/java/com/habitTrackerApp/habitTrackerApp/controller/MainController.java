35d8cdf0e9f12ebe6fc759bfc9b00bad7dee0f19
mr-arzh <asiris.joy@icloud.com>
2025-09-24 22:37:42 +0300

    feat: created rest-controllers SecurityController and MainController

---
+++ src/main/java/com/habitTrackerApp/habitTrackerApp/controller/MainController.java
@@ -0,0 +1,9 @@
+package com.habitTrackerApp.habitTrackerApp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/secured")
+public class MainController { //выдергиваем инфу по юзеру в клиент
+}
